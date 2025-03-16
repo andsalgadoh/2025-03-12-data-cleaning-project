2da8df09cee2cc3f9d3f964433bfdf33a34218bb
Andres Salgado <andsalgadoh@hotmail.com>
2025-03-16 04:32:08 -0300

    Added realistic synthetic irradiance and visualizations

--- main.py
+++ main.py
@@ -8,20 +8,46 @@ import scripts.synthetic_data_generation as sdg
 # Generate synthetic irradiance data:
 times = pd.date_range(start="2025-03-01", end="2025-03-07", freq="min")
 clearsky_irradiance = sdg.GenerateIrradiance.clearsky(times)
-ghi = clearsky_irradiance["ghi"]
+clearsky_ghi = clearsky_irradiance["ghi"]
 
 # Visualize synthetic irradiance data:
+print("Visualizing synthetic clearsky irradiance")
 plt.figure(1)
-plt.plot(times, ghi)
-plt.show()
+plt.subplot(4,1,1)
+plt.title("Synthetic clearsky irradiance")
+plt.plot(times, clearsky_ghi)
+plt.show(block=False)
 
-# Add gausian noise to the data and visualize it
-noisy_ghi = sdg.GenerateIrradiance.add_noise(ghi)
+# Add sensor malfunction examples to the data and visualize:
+print("Visualizing sensor malfunction")
+sensor_ghi = sdg.GenerateIrradiance.add_sensor_disconnect(clearsky_ghi)
 plt.figure(1)
-plt.plot(times, noisy_ghi)
-plt.show()
+plt.subplot(4,1,2)
+plt.title("Adding sensor malfunction")
+plt.plot(times, sensor_ghi, ".", markersize=2)
+plt.tight_layout()
+plt.show(block=False)
 
-# Add extreme outliers to the data
+# Add extreme outliers to the data and visualize:
+print("Visualizing extreme outliers")
+extreme_ghi = sdg.GenerateIrradiance.add_extreme_outliers(sensor_ghi)
+plt.figure(1)
+plt.subplot(4,1,3)
+plt.title("Adding outliers")
+plt.plot(times, extreme_ghi, ".", markersize=2)
+plt.show(block=False)
+
+# Add gausian noise to the data and visualize everything:
+print("Visualizing noisy synthetic irradiance")
+realistic_ghi = sdg.GenerateIrradiance.add_noise(extreme_ghi)
+plt.figure(1)
+plt.subplot(4,1,4)
+plt.title("Realistic clear-sky irradiance")
+plt.plot(times, realistic_ghi, ".", markersize=2)
+plt.show(block=True)
+
+
+
 # df = load_dataset("data/private/private_data_raw.csv")
 
 # # t= df["Time"].iloc[1440:1440*7]
--- scripts/synthetic_data_generation.py
+++ scripts/synthetic_data_generation.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pvlib
 
 class GenerateIrradiance:
+    
     def clearsky(times,
             latitude = -41.13941227780086,
             longitude = -73.02542294598776,
@@ -18,11 +19,52 @@ class GenerateIrradiance:
         return location.get_clearsky(times,
                                      model="ineichen")
                                          
-    def add_noise(irradiance_timeseries):
-        print("Function hasn't been coded yet")
+    def add_noise(irradiance_ts, noise_level=0.02):
+        # Adds random noise to an irradiance timeseries
+        std = noise_level * np.mean(irradiance_ts)
+        rng = np.random.default_rng()
+        noise = rng.normal(loc=0,
+                           scale=std,
+                           size=len(irradiance_ts))
+        return irradiance_ts + noise
     
-    def add_sensor_disconnect(irradiance_timeseries):
-        print("Function hasn't been coded yet")
+    def add_extreme_outliers(
+            irradiance_ts,
+            outlier_rate=0.01,
+            value_multiplier=5):
+        # Introduces extreme outliers at random positions
+        rng = np.random.default_rng()
+        length = len(irradiance_ts)
+        num_outliers = round(length * outlier_rate)
+        indices = rng.choice(a=length,
+                             size=num_outliers
+                             )
+        irradiance_ts.iloc[indices] = np.abs(irradiance_ts.iloc[indices] * value_multiplier)
+        return irradiance_ts
 
-    def add_extreme_outliers(irradiance_timeseries):
-        print("Function hasn't been coded yet")
+    def add_sensor_disconnect(
+            irradiance_ts,
+            total_fail_rate=0.1,
+            num_events=5,
+            ):
+        # Introduces continuous linear drift to simulate sensor disconnections
+
+        length = len(irradiance_ts)
+        fail_length = round(length * (total_fail_rate / num_events))
+
+        rng = np.random.default_rng()
+        indices = rng.choice(a=length,
+                             size=num_events
+                             )  # indices is an ndarray
+        
+        # Linear drift
+        for index in indices:
+            x0 = index
+            y0 = irradiance_ts.iloc[index]
+            m = 0.01 * rng.random(1) * np.mean(irradiance_ts)
+
+            x = np.arange(index, index + fail_length)
+            y = m*(x - x0) + y0
+            irradiance_ts.iloc[x] = y
+        
+        return irradiance_ts
