346926d1be492aec428da2e81c51bb972047377c
Andres Salgado <andsalgadoh@hotmail.com>
2025-03-16 19:21:40 -0300

    Revamped synthetic irradiance generation

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ import scripts.synthetic_data_generation as sdg
 
 # Generate synthetic irradiance data:
 times = pd.date_range(start="2025-03-01", end="2025-03-07", freq="min")
-clearsky_irradiance = sdg.GenerateIrradiance.clearsky(times)
-clearsky_ghi = clearsky_irradiance["ghi"]
+ghi = sdg.SyntheticIrradiance(times)  # Initialized with a clearsky model
 
 # Visualize synthetic irradiance data:
+clearsky_ghi = ghi.series
+
 print("Visualizing synthetic clearsky irradiance")
 plt.figure(1)
 plt.subplot(4,1,1)
@@ -19,33 +20,37 @@ plt.title("Synthetic clearsky irradiance")
 plt.plot(times, clearsky_ghi)
 plt.show(block=False)
 
-# Add extreme outliers to the data and visualize:
-print("Visualizing extreme outliers")
-extreme_ghi = sdg.GenerateIrradiance.add_extreme_outliers(clearsky_ghi)
+# Add sensor malfunction examples to the data and visualize:
+sensor_ghi = ghi.add_sensor_disconnect()
+
+print("Visualizing sensor malfunction")
 plt.figure(1)
 plt.subplot(4,1,2)
-plt.title("Adding outliers")
-plt.plot(times, extreme_ghi, ".", markersize=2)
-plt.show(block=False)
-
-# Add sensor malfunction examples to the data and visualize:
-print("Visualizing sensor malfunction")
-sensor_ghi = sdg.GenerateIrradiance.add_sensor_disconnect(extreme_ghi)
-plt.figure(1)
-plt.subplot(4,1,3)
 plt.title("Adding sensor malfunction")
 plt.plot(times, sensor_ghi, ".", markersize=2)
 plt.tight_layout()
 plt.show(block=False)
 
 # Add gausian noise to the data and visualize everything:
+noisy_ghi = ghi.add_noise()
+
 print("Visualizing noisy synthetic irradiance")
-realistic_ghi = sdg.GenerateIrradiance.add_noise(sensor_ghi)
+plt.figure(1)
+plt.subplot(4,1,3)
+plt.title("Adding noise")
+plt.plot(times, noisy_ghi, ".", markersize=2)
+plt.show(block=False)
+
+# Add extreme outliers to the data and visualize:
+realistic_ghi = ghi.add_outliers()
+
+print("Visualizing extreme outliers")
 plt.figure(1)
 plt.subplot(4,1,4)
-plt.title("Realistic clear-sky irradiance (adding noise)")
+plt.title("Adding outliers")
 plt.plot(times, realistic_ghi, ".", markersize=2)
-plt.show(block=True)
+plt.show(block=False)
+
 
 # Save synthetic data to .csv file
 dataset_name = "data/public/realistic_ghi_data.csv"
@@ -54,49 +59,10 @@ if not os.path.isfile(dataset_name):
     save_confirm = input("Would you like to save this one? (Y/N): ")
 else:
     print(f"A synthetic irradiance dataset already exist in {dataset_name}")
-    save_confirm = input("Do you want to overwrite it? (Y/N): ")
+    save_confirm = input("Do you want to overwrite it? (Y/N): ").upper()
 
 if save_confirm == "Y":
     print(f"Saving dataset in {dataset_name}")
     df = pd.DataFrame({"Timestamp": times, "GHI": realistic_ghi})
     df.set_index("Timestamp", inplace=True)
     df.to_csv(dataset_name)
-
-# Notes: "realistic_ghi" is of type pandas.core.series.Series
-#        "times" is of type DatetimeIndex
-
-
-
-
-
-# df = load_dataset("data/private/private_data_raw.csv")
-
-# # t= df["Time"].iloc[1440:1440*7]
-# # power = df["power"].iloc[1440:1440*7]
-# # temperature = df["temperature"].iloc[1440:1440*7]
-
-# t= df["Time"]
-# power = df["power"]
-# temperature = df["temperature"]
-
-# plt.figure(figsize=(10,5))
-# plt.plot(t, power)
-# plt.plot(t, temperature)
-# plt.xlabel("Time")
-# plt.ylabel("Power")
-# plt.title("Power over Time")
-# plt.xticks([])
-# plt.show()
-
-# # plt.plot(df["date"], df["power"])
-# # plt.xlabel("Time")
-# # plt.ylabel("Power")
-# # plt.title("Power over Time")
-# # plt.show()
-
-
-# # # Example dataset
-# # data = [25, 26, 1000, 27, 28, -500, 29]
-# # anomalies = detect_anomalies(data, tol=1.0)
-
-# # print(anomalies)  # Boolean mask for anomalies
--- scripts/synthetic_data_generation.py
+++ scripts/synthetic_data_generation.py
@@ -3,68 +3,119 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pvlib
 
-class GenerateIrradiance:
+class SyntheticIrradiance:
     
-    def clearsky(times,
-            latitude = -41.13941227780086,
-            longitude = -73.02542294598776,
-            tz = -4,
-            name = "Frutillar"
-    ):
-        location = pvlib.location.Location(latitude,
-                                           longitude,
-                                           tz,
-                                           name=name)
-        # Function returns a dataframe of ghi, dni, dhi
-        return location.get_clearsky(times,
-                                     model="ineichen")
-                                         
-    def add_noise(irradiance_ts, noise_level=0.02):
-        # Adds random noise to an irradiance timeseries
-        std = noise_level * np.mean(irradiance_ts)
-        rng = np.random.default_rng()
-        noise = rng.normal(loc=0,
-                           scale=std,
-                           size=len(irradiance_ts))
-        return irradiance_ts + noise
-    
-    def add_extreme_outliers(
-            irradiance_ts,
-            outlier_rate=0.01,
-            value_multiplier=5):
-        # Introduces extreme outliers at random positions
-        rng = np.random.default_rng()
-        length = len(irradiance_ts)
-        num_outliers = round(length * outlier_rate)
-        indices = rng.choice(a=length,
-                             size=num_outliers
-                             )
-        irradiance_ts.iloc[indices] = np.abs(irradiance_ts.iloc[indices] * value_multiplier)
-        return irradiance_ts
-
-    def add_sensor_disconnect(
-            irradiance_ts,
-            total_fail_rate=0.15,
-            num_events=4,
+    def __init__(
+            self,
+            times,
+            location = {"latitude": -41.13941227780086,
+                        "longitude": -73.02542294598776,
+                        "timezone": -4,
+                        "name": "Frutillar"},
+            irradiance_type = "ghi"
             ):
+        self.times = times
+        self.location = location
+        self.irradiance_type = irradiance_type
+
+        self.series = np.zeros(len(times))
+        self.add_clearsky()
+        """ The purpose of self.series is to provide
+            easy access to the finished build of the series.
+
+            For extended use, the class provides the components:
+            - self.clearsky - Through the add_clearsky method
+            - self.noise    - Through the add_noise method
+
+            Adding outliers and sensor malfunction replace values
+            in the series "irreversibly"
+            - self.add_outliers
+            - self.add_malfunction
+        """
+
+        self.outlier_mask = np.zeros_like(self.series, dtype=bool)
+        self.malfunction_mask = np.zeros_like(self.series, dtype=bool)
+
+    def add_clearsky(self):
+        location = pvlib.location.Location(
+                                self.location["latitude"],
+                                self.location["longitude"],
+                                self.location["timezone"],
+                                name=self.location["name"]
+                                )
+        
+        # pvlib's get_clearsky returns a dataframe of ghi, dni, dhi
+        components = location.get_clearsky(self.times,
+                                           model="ineichen")
+
+        # Define the clearsky component and update
+        self.clearsky = components[self.irradiance_type]
+        self.series = self.series + self.clearsky
+        return self.series
+                                         
+    def add_noise(self,
+                  noise_level=0.01):
+        std = noise_level * np.max(self.clearsky)
+        rng = np.random.default_rng()
+        
+        # Define the noise component and update
+        self.noise = rng.normal(loc=0,
+                                scale=std,
+                                size=len(self.times))
+        self.series = self.series + self.noise
+        return self.series
+    
+    def add_outliers(self,
+                     outlier_rate=0.01,
+                     amplifier=3):
+        # Introduces outliers at random positions
+        num_outliers = round(outlier_rate * len(self.series))
+        rng = np.random.default_rng()
+        indices = rng.choice(a=len(self.series),
+                             size=num_outliers)
+        
+        # Prevent outliers from affecting malfunction behavior
+        self.outlier_mask[indices] = True
+        self.outlier_mask = self.outlier_mask & ~ self.malfunction_mask
+        
+        # Update series
+        self.series.loc[self.outlier_mask] = np.abs(self.clearsky.loc[self.outlier_mask]
+                                                * amplifier)
+        return self.series
+
+    def add_sensor_disconnect(self,
+                              disconnect_ratio=0.15,
+                              num_events=4,
+                             ):
+        pass
         # Introduces continuous linear drift to simulate sensor disconnections
 
-        length = len(irradiance_ts)
-        fail_length = round(length * (total_fail_rate / num_events))
+        fail_length = round(len(self.series) * (disconnect_ratio / num_events))
 
         rng = np.random.default_rng()
-        indices = rng.choice(a=length,
-                             size=num_events
-                             )  # indices is an ndarray
-        
-        # Linear drift
+        # Check indices are distanced from each other (10 attempts)
+        for count in range(1,11):
+            indices = rng.choice(a = len(self.series) - fail_length,
+                                 size=num_events)
+            indices = np.sort(indices)
+            d_idx = np.diff(indices)
+
+            if all(np.abs(d_idx) >= 2 * fail_length):
+                break
+            
+        # Linear drift (starts from clearsky model)
         for index in indices:
             x0 = index
-            y0 = irradiance_ts.iloc[index]
-            m = 0.01 * rng.random(1) * np.mean(irradiance_ts)
+            y0 = self.clearsky.iloc[index]
+            m = 0.005 * rng.random(1) * np.mean(self.clearsky)
             x = np.arange(index, index + fail_length)
-            x = x[x < length]  # Prevents attempt to access inexistent values
+            x = x[x < len(self.clearsky)]  # Prevents attempt to access inexistent values
             y = m*(x - x0) + y0
-            irradiance_ts.iloc[x] = y
-        
-        return irradiance_ts
+
+            # Update masks
+            self.malfunction_mask[x] = True
+            self.outlier_mask[x] = False
+
+            # Update series
+            self.series.iloc[x] = y
+        return self.series
